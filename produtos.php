343ee99283e43b09252c7bb54fc0b5c68f56f48a
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2014-04-19 12:52:54 -0300

    small changes

---
+++ produtos.php
@@ -0,0 +1,12 @@
+<?php
+
+produtos = 1, camera, "imagens/camera.jpg", preco,
+produtos = 2, celular,"imagens/celular.jpg"
+produtos = 3, impressora, "imagens/impressora.jpg"
+
+relacionamentos
+1 2 
+1 3
+
+
+?>
